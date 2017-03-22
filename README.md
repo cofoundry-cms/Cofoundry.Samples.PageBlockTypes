a9034b80ca75ffff47e0cd8661021470194f9a50
Joel Mitchell <me@joelmitchell.co.uk>
2017-03-22 16:07:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cofoundry.Samples.PageModules
+
+A bare website showing various examples of page module types.
+
+If you have any requests for example module types, [let us know](https://github.com/cofoundry-cms/cofoundry/wiki/Feedback-&-Community).
+
+#### To get started:
+
+1. Create a database named 'Cofoundry.Samples.PageModules' and check the Cofoundry connection string in the web.config file is correct for you sql server instance
+2. Run the website and navigate to *"/admin"*, which will display the setup screen
+3. Enter an application name and setup your user account. Submit the form to complete the site setup. 
+4. Log in and add a page with the *General* template, click on save and edit to go to the visual editor and play around with the page modules.
+
+####  Example Page Module Types:
+
+- **DirectoryList:** Lists pages in a specific directory. Demonstrates searching for pages using `IPageRepository` and using the `WebDirectoryAttribute` data model attribute.
+- **PageList:** An orderable list of links to pages. Demonstrates quering for caches page routes using `IPageRepository`, the `PageCollectionAttribute` data model attribute and generating links to pages from page objects.
+- **PageSnippet:** Displays summary information about a page. Demonstrates the `PageAttribute` data model attribute, querying and manipulating module data and handling availability of linked entities (due to draft status).
+
+
+
---
+++ src/Cofoundry.Samples.PageModules/Cofoundry/PageModules/DirectoryList/DirectoryListDisplayModelMapper.cs
@@ -0,0 +1,63 @@
+﻿using Cofoundry.Core;
+using Cofoundry.Core.Web;
+using Cofoundry.Domain;
+using Cofoundry.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cofoundry.Samples.PageModules
+{
+    /// <summary>
+    /// A IPageModuleDisplayModelMapper class handles the mapping from
+    /// a display model to a data model.
+    /// 
+    /// The mapper supports DI which gives you flexibility in what data
+    /// you want to include in the display model and how you want to 
+    /// map it. Mapping is done in batch to improve performance when 
+    /// the same module type is used multiple times on a page
+    /// </summary>
+    public class DirectoryListDisplayModelMapper : IPageModuleDisplayModelMapper<DirectoryListDataModel>
+    {
+        private readonly IPageRepository _pageRepository;
+
+        public DirectoryListDisplayModelMapper(
+            IPageRepository pageRepository
+            )
+        {
+            _pageRepository = pageRepository;
+        }
+
+        public IEnumerable<PageModuleDisplayModelMapperOutput> Map(
+            IEnumerable<PageModuleDisplayModelMapperInput<DirectoryListDataModel>> inputs,
+            WorkFlowStatusQuery workflowStatus
+            )
+        {
+            foreach (var input in inputs)
+            {
+                var output = new DirectoryListDisplayModel();
+
+                // There's no batch search method so we need to do this for each input
+                var query = new SearchPageSummariesQuery();
+                query.WebDirectoryId = input.DataModel.WebDirectoryId;
+                query.PageSize = input.DataModel.PageSize;
+
+                // Pass through the workflow status so that we only show published pages 
+                // when viewing the live site.
+                // NB: The page search api is essentially borrowed from the admin panel
+                // and could be improved upon for this type of querying. See issue #87
+                if (workflowStatus == WorkFlowStatusQuery.Published)
+                {
+                    query.WorkFlowStatus = WorkFlowStatus.Published;
+                }
+
+                output.Pages = _pageRepository.SearchPageSummaries(query);
+
+                // The CreateOutput() method wraps the mapped display 
+                // model with it's identifier so we can identify later on
+                yield return input.CreateOutput(output);
+            }
+        }
+    }
+}
