a3cce986687645f2b6b91a090d42c4e8f252f3ec
Joel Mitchell <jmail.mitchell@elvislondon.com>
2021-07-26 09:51:31 +0100

    Added example of a block type hosted in a shared project.

---
+++ Cofoundry.Samples.PageBlockTypes.ExampleSharedProject/AssemblyResourceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Cofoundry.Core.ResourceFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cofoundry.Samples.PageBlockTypes.ExampleSharedProject
+{
+    /// <summary>
+    /// Registers this assembly so that embedded resources (e.g. views/css/js) 
+    /// can be picked up. No implementation is required, this is just a marker
+    /// class that is automatically detected by Cofoundry.
+    /// </summary>
+    public class AssemblyResourceRegistration : IAssemblyResourceRegistration
+    {
+    }
+}
